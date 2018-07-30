f66ed527d0c2af074fd3ee81fcc2a173c310f30a
iMR Framework <37110500+imr-framework@users.noreply.github.com>
2018-07-30 17:24:41 -0400

    Revert "Matlab"

--- MATLAB/ROCKET/Code/utils/checkfix_k.m
+++
@@ -1,8 +0,0 @@
-function kx = checkfix_k(kx,delk_max)
-
-ind = diff(kx)>delk_max;
-if(sum(ind) >0)
-    for k = 1:length(ind)
-        
-    end
-end
--- MATLAB/ROCKET/Code/utils/get_spiral_waveforms.m
+++
@@ -1,5 +0,0 @@
-function [ktraj, G] = get_spiral_waveforms()
-
-load ('Spirals_JonUoM_stack');
-figure; plot(T,gx,'r'); hold on; plot(T,gy,'g'); plot(T,gz,'b');
-xlabel('time (msec)'); ylabel('G/cm');
